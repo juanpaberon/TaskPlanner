c15a4abbaa4539d930765a7d64d8351447c4c401
jberon3 <jberon3@gatech.edu>
2024-04-04 11:43:37 -0500

    add dao interface for Task

---
+++ src/main/java/com/database/TaskDao.java
@@ -0,0 +1,15 @@
+package com.database;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.model.Task;
+
+public interface TaskDao {
+    
+    public int add(Task task) throws SQLException;
+    public void delete(int taskID) throws SQLException;
+    public Task getTask(int taskID) throws SQLException;
+    public List<Task> getTasks() throws SQLException;
+    public void update(Task task) throws SQLException;
+}
