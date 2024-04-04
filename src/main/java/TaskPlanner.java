a1993d61aa9607da9edca61cb788fb3610d5ff2e
jberon3 <jberon3@gatech.edu>
2024-04-04 14:35:41 -0500

    add taskdaoimplementation for add and get task from database

--- src/main/java/TaskPlanner.java
+++ src/main/java/TaskPlanner.java
@@ -1,38 +1,43 @@
-import com.database.DatabaseConnection;
+import com.database.TaskDaoImplementation;
+import com.model.Task;
 
-import java.sql.Statement;
-import java.sql.Connection;
-import java.sql.Timestamp;
-import java.sql.ResultSet;
+import java.sql.*;
+import java.time.LocalDateTime;
 
 public class TaskPlanner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Hello World");
 
-        Connection con = DatabaseConnection.getConnection();
+        TaskDaoImplementation taskDao = new TaskDaoImplementation();
 
-        try {
-            Statement statement;
-            statement = con.createStatement();
-            ResultSet resultSet;
-            resultSet = statement.executeQuery(
-                    "SELECT * FROM Task");
-            int taskId;
-            String taskName;
-            Timestamp timeCreated;
-            while (resultSet.next()) {
-                taskId = resultSet.getInt("ID");
-                taskName = resultSet.getString("name").trim();
-                timeCreated = resultSet.getTimestamp("timeCreated");
-                System.out.println("id : " + taskId
-                        + " name : " + taskName + " timeCreated:" + timeCreated);
-            }
-            resultSet.close();
-            statement.close();
-        }
-        catch (Exception exception) {
-            System.out.println(exception);
-        }
+        int taskID = 1;
+        Task task = taskDao.getTask(taskID);
+        System.out.println(task);
+
+//        Connection con = DatabaseConnection.getConnection();
+//
+//        try {
+//            Statement statement;
+//            statement = con.createStatement();
+//            ResultSet resultSet;
+//            resultSet = statement.executeQuery(
+//                    "SELECT * FROM Task");
+//            int taskId;
+//            String taskName;
+//            LocalDateTime timeCreated;
+//            while (resultSet.next()) {
+//                taskId = resultSet.getInt("ID");
+//                taskName = resultSet.getString("name").trim();
+//                timeCreated = resultSet.getTimestamp("timeCreated").toLocalDateTime();
+//                System.out.println("id : " + taskId
+//                        + " name : " + taskName + " timeCreated:" + timeCreated);
+//            }
+//            resultSet.close();
+//            statement.close();
+//        }
+//        catch (Exception exception) {
+//            System.out.println(exception);
+//        }
     }
 }
--- src/main/java/com/database/TaskDao.java
+++ src/main/java/com/database/TaskDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 import com.model.Task;
 
 public interface TaskDao {
-    
+
     public int add(Task task) throws SQLException;
     public void delete(int taskID) throws SQLException;
     public Task getTask(int taskID) throws SQLException;
