5ae3df06625009c077d345ccb24e48957aef067d
jberon3 <jberon3@gatech.edu>
2024-04-09 16:59:05 -0500

    add option to add due date and due time, but it is only in the gui

--- src/main/java/com/gui/AddNewTask.java
+++ src/main/java/com/gui/AddNewTask.java
@@ -18,8 +18,15 @@ public class AddNewTask extends JPanel implements ActionListener {
     JPanel inputNamePanel = new JPanel();
     JPanel buttonPanel = new JPanel();
     JPanel dueDatePanel = new JPanel();
+    ButtonGroup dueDateEnable = new ButtonGroup();
+    JRadioButton yesRadioButtonDueDate = new JRadioButton("Yes");
+    JRadioButton noRadioButtonDueDate = new JRadioButton("No");
+    JTextField dueDate = new JTextField();
     JPanel dueTimePanel = new JPanel();
-    JLabel nameLabel = new JLabel();
+    ButtonGroup dueTimeEnable = new ButtonGroup();
+    JRadioButton yesRadioButtonDueTime = new JRadioButton("Yes");
+    JRadioButton noRadioButtonDueTime = new JRadioButton("No");
+    JTextField dueTime = new JTextField();
     JTextField nameTextField = new JTextField();
     JButton button = new JButton("Add task");
 
@@ -38,21 +45,22 @@ public class AddNewTask extends JPanel implements ActionListener {
     }
 
     private void inputNamePanelSetUp() {
-        inputNamePanel.setBounds(0,0, 100,50);
-        inputNamePanel.setLayout(new BorderLayout());
+        inputNamePanel.setBounds(0,0, 125,100);
+        inputNamePanel.setLayout(new FlowLayout(FlowLayout.LEADING,0,-2));
         this.add(inputNamePanel);
 
+        JLabel nameLabel = new JLabel();
         nameLabel.setPreferredSize(new Dimension(100,25));
         nameLabel.setText("Task Name");
-        inputNamePanel.add(nameLabel, BorderLayout.NORTH);
+        inputNamePanel.add(nameLabel);
 
         nameTextField.setPreferredSize(new Dimension(100,25));
         nameTextField.setText("Name");
-        inputNamePanel.add(nameTextField, BorderLayout.SOUTH);
+        inputNamePanel.add(nameTextField);
     }
 
     private void buttonPanelSetUp() {
-        buttonPanel.setBounds(0,50,400,100);
+        buttonPanel.setBounds(0,75,400,100);
         buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
         buttonPanel.add(button);
         this.add(buttonPanel);
@@ -60,15 +68,47 @@ public class AddNewTask extends JPanel implements ActionListener {
     }
 
     private void dueDatePanelSetUp() {
-        dueDatePanel.setBounds(0,100, 100, 50);
-        dueDatePanel.setLayout(new BorderLayout());
+        dueDatePanel.setBounds(125,0, 125, 100);
+        dueDatePanel.setLayout(new FlowLayout(FlowLayout.LEADING,0,-2));
         this.add(dueDatePanel);
+
+        JLabel dueDateEnableLabel = new JLabel("Add due date");
+        dueDateEnableLabel.setPreferredSize(new Dimension(100,25));
+        dueDatePanel.add(dueDateEnableLabel);
+
+        yesRadioButtonDueDate.setPreferredSize(new Dimension(60,25));
+        dueDatePanel.add(yesRadioButtonDueDate);
+        dueDateEnable.add(yesRadioButtonDueDate);
+
+        noRadioButtonDueDate.setPreferredSize(new Dimension(50,25));
+        dueDatePanel.add(noRadioButtonDueDate);
+        dueDateEnable.add(noRadioButtonDueDate);
+
+        dueDate.setPreferredSize(new Dimension(120,25));
+        dueDate.setText("YYYY.MM.DD");
+        dueDatePanel.add(dueDate);
     }
 
     private void dueTimePanelSetUp() {
-        dueTimePanel.setBounds(0,200, 100, 50);
-        dueTimePanel.setLayout(new BorderLayout());
+        dueTimePanel.setBounds(250,0, 125, 100);
+        dueTimePanel.setLayout(new FlowLayout(FlowLayout.LEADING,0,-2));
         this.add(dueTimePanel);
+
+        JLabel dueTimeEnableLabel = new JLabel("Add due time");
+        dueTimeEnableLabel.setPreferredSize(new Dimension(100,25));
+        dueTimePanel.add(dueTimeEnableLabel);
+
+        yesRadioButtonDueTime.setPreferredSize(new Dimension(60,25));
+        dueTimePanel.add(yesRadioButtonDueTime);
+        dueTimeEnable.add(yesRadioButtonDueTime);
+
+        noRadioButtonDueTime.setPreferredSize(new Dimension(50,25));
+        dueTimePanel.add(noRadioButtonDueTime);
+        dueTimeEnable.add(noRadioButtonDueTime);
+
+        dueTime.setPreferredSize(new Dimension(120,25));
+        dueTime.setText("HH:MM");
+        dueTimePanel.add(dueTime);
     }
 
     @Override
