93ced749534307f453b39128a485cbc5776a9df0
jberon3 <jberon3@gatech.edu>
2024-04-30 09:08:27 -0500

    sort display task by due date

--- src/main/java/com/gui/displayTask/Displayer.java
+++ src/main/java/com/gui/displayTask/Displayer.java
@@ -7,8 +7,12 @@ import com.model.Task;
 import javax.swing.*;
 import java.awt.*;
 import java.sql.SQLException;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Displayer extends JScrollPane {
@@ -54,6 +58,8 @@ public class Displayer extends JScrollPane {
         }
 
         createMainPanel(allTasks.size());
+        allTasks.sort(new TaskComparator());
+        Collections.reverse(allTasks);
 
         int counter = 0;
         for (Task task : allTasks) {
@@ -76,5 +82,47 @@ public class Displayer extends JScrollPane {
         this.panel.add(taskPanel);
     }
 
+}
 
+class TaskComparator implements java.util.Comparator<Task> {
+    @Override
+    public int compare(Task a, Task b) {
+        LocalDateTime dueDateTimeA = processDueDateTime(a);
+        LocalDateTime dueDateTimeB = processDueDateTime(b);
+        if (dueDateTimeA != null & dueDateTimeB != null) {
+            return (int) Duration.between(dueDateTimeA, dueDateTimeB).getSeconds();
+        } else if (dueDateTimeA == null & dueDateTimeB != null) {
+            return 1;
+        } else if (dueDateTimeA != null) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    private LocalDateTime processDueDateTime(Task a) {
+        LocalDate dueDate = a.getDueDate();
+        LocalTime dueTime = a.getDueTime();
+
+        LocalDateTime dueDateTime = null;
+        if (dueDate != null & dueTime != null){
+            dueDateTime = LocalDateTime.of(
+                    dueDate.getYear(), dueDate.getMonthValue(), dueDate.getDayOfMonth(),
+                    dueTime.getHour(), dueTime.getMinute(), dueTime.getSecond()
+                    );
+        } else if (dueDate != null & dueTime == null){
+            dueDateTime = LocalDateTime.of(
+                    dueDate.getYear(), dueDate.getMonthValue(), dueDate.getDayOfMonth(),
+                    23, 59, 59
+            );
+        } else if (dueTime != null) {
+            LocalDateTime creationTime = a.getTimeCreated();
+            dueDateTime = LocalDateTime.of(
+                    creationTime.getYear(), creationTime.getMonthValue(), creationTime.getDayOfMonth(),
+                    dueTime.getHour(), dueTime.getMinute(), dueTime.getSecond()
+            );
+        }
+
+        return dueDateTime;
+    }
 }
