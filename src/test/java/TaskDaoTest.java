1f3dfb41541476650f363f847f52c8e683b3b7af
jberon3 <jberon3@gatech.edu>
2024-05-03 13:21:53 -0500

    add update task test

--- src/test/java/TaskDaoTest.java
+++ src/test/java/TaskDaoTest.java
@@ -5,7 +5,9 @@ import com.database.TaskDaoImplementation;
 import com.model.Task;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 public class TaskDaoTest {
@@ -19,14 +21,18 @@ public class TaskDaoTest {
         }
     }
 
-    @Test
-    void addTaskTest() throws SQLException {
-        TaskDaoImplementation taskDao = new TaskDaoImplementation("Test");
-
+    void addTaskDatabase01(TaskDaoImplementation taskDao) throws SQLException {
         String newTaskName = "Change oil";
         LocalDateTime newTaskCreationTime = LocalDateTime.of(2024,4,5,15,30);
         Task newTask = new Task(-1, newTaskName, newTaskCreationTime);
         taskDao.add(newTask);
+    }
+
+    @Test
+    void addTaskTest() throws SQLException {
+        TaskDaoImplementation taskDao = new TaskDaoImplementation("Test");
+
+        addTaskDatabase01(taskDao);
 
         List<Task> allTasks = taskDao.getTasks();
         Assertions.assertEquals(1, allTasks.size());
@@ -38,16 +44,44 @@ public class TaskDaoTest {
     void deleteTaskTest() throws SQLException {
         TaskDaoImplementation taskDao = new TaskDaoImplementation("Test");
 
-        String newTaskName = "Change oil";
-        LocalDateTime newTaskCreationTime = LocalDateTime.of(2024,4,5,15,30);
-        Task newTask = new Task(-1, newTaskName, newTaskCreationTime);
-        taskDao.add(newTask);
+        addTaskDatabase01(taskDao);
 
         List<Task> allTasks = taskDao.getTasks();
         Task task = allTasks.get(0);
         taskDao.delete(task.getID());
         allTasks = taskDao.getTasks();
         Assertions.assertEquals(0, allTasks.size());
+        cleanDatabase();
+    }
+
+
+    @Test
+    void updateTaskTest() throws SQLException {
+        TaskDaoImplementation taskDao = new TaskDaoImplementation("Test");
+
+        addTaskDatabase01(taskDao);
+        List<Task> allTasks = taskDao.getTasks();
+        Task task = allTasks.get(0);
+
+        LocalTime dueTime = LocalTime.of(12,5,10);
+        task.setDueTime(dueTime);
+
+        LocalDate dueDate = LocalDate.of(2024,4,25);
+        task.setDueDate(dueDate);
+
+        LocalDateTime timeFinished = LocalDateTime.of(2024,4,29,23,25,25);
+        task.setTimeFinished(timeFinished);
+
+        taskDao.update(task);
+
+        List<Task> allTasksDatabase = taskDao.getTasks();
+        Task taskDatabase = allTasksDatabase.get(0);
+
+        Assertions.assertNotEquals(task, taskDatabase);
+        Assertions.assertEquals(task.getDueTime(), taskDatabase.getDueTime());
+        Assertions.assertEquals(task.getDueDate(), taskDatabase.getDueDate());
+        Assertions.assertEquals(task.getTimeFinished(), taskDatabase.getTimeFinished());
+
         cleanDatabase();
     }
 }
